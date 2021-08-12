5167d9457d4fa1edcceabb47257cf619b9b36f33
ELKIN JAVIER PAREDES LLUMIPANTA <elkin.paredes.llumipanta@estudiantes.cordillera.edu.ec>
2021-08-12 16:45:50 -0500

    Cargar todo

---
+++ ProyectoIntegrador_4B/Menu_Almuerzo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ProyectoIntegrador_4B
+{
+    [Activity(Label = "ProyectoIntegrador", MainLauncher = true)]
+    public class Menu_Almuerzo : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Menu_Almuerzo);
+        }
+    }
+}
---
+++ ProyectoIntegrador_4B/Registrar_Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ProyectoIntegrador_4B
+{
+    [Activity(Label = "Registrar_Usuario",MainLauncher =false)]
+    public class Registrar_Usuario : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Registrar_Usuario);
+        }
+    }
+}
